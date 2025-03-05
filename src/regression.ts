6bb6a5cb93e8c6c7fa88a885802884822e97a4cf
null8626 <vierofernando9@gmail.com>
2025-03-05 16:09:01 +0700

    feat: add interpolation and regression

---
+++ src/regression.ts
@@ -0,0 +1,112 @@
+export interface LinearRegressionOutput {
+  m: number;
+  b: number;
+}
+
+export interface PolynomialRegressionOutput {
+  a: number;
+  b: number;
+  c: number;
+}
+
+export interface RegressionOutput {
+  linear: LinearRegressionOutput;
+  polynomial: PolynomialRegressionOutput;
+}
+
+function computePolynomialRegressionMatrix(matrix: number[]) {
+  let pivot, eliminationPivot;
+
+  for (let i = 0, j, k; i < 3; i++) {
+    const pivotStartIndex = i * 4;
+    pivot = matrix[pivotStartIndex + i]!;
+
+    for (j = 0; j < 4; j++) {
+      matrix[pivotStartIndex + j]! /= pivot;
+    }
+
+    for (j = 0; j < 3; j++) {
+      if (j == i) {
+        continue;
+      }
+
+      const nonPivotStartIndex = j * 4;
+      eliminationPivot = matrix[nonPivotStartIndex + i]!;
+
+      for (k = i; k < 4; k++) {
+        matrix[nonPivotStartIndex + k]! -=
+          eliminationPivot * matrix[pivotStartIndex + k]!;
+      }
+    }
+  }
+}
+
+/**
+ * @param {number[][]} input Regression input, must be an array of [x, y]
+ *
+ * @returns {RegressionOutput} Regression result for both linear and polynomial regression
+ */
+export function regression(input: number[][]): RegressionOutput {
+  if (input.length === 0) {
+    throw new Error('Input array must not be empty.');
+  }
+
+  let x,
+    y,
+    m_divisor,
+    sumX = 0,
+    sumY = 0,
+    sumXY = 0,
+    sumXSquared = 0,
+    sumXCubed = 0,
+    sumXPower4 = 0,
+    sumXSquaredY = 0;
+
+  for (let index = 0; index < input.length; index++) {
+    x = input[index]![0]!;
+    y = input[index]![1]!;
+
+    sumX += x;
+    sumY += y;
+    sumXY += x * y;
+    sumXSquared += x * x;
+    sumXCubed += x * x * x;
+    sumXPower4 += x * x * x * x;
+    sumXSquaredY += x * x * y;
+  }
+
+  const m =
+    (m_divisor = input.length * sumXSquared - sumX * sumX) === 0
+      ? 0
+      : (input.length * sumXY - sumX * sumY) / m_divisor;
+  const b = (sumY - m * sumX) / input.length;
+
+  const polMatrix = [
+    input.length,
+    sumX,
+    sumXSquared,
+    sumY,
+    sumX,
+    sumXSquared,
+    sumXCubed,
+    sumXY,
+    sumXSquared,
+    sumXCubed,
+    sumXPower4,
+    sumXSquaredY,
+  ];
+
+  computePolynomialRegressionMatrix(polMatrix);
+
+  return {
+    linear: {
+      m,
+      b,
+    },
+    polynomial: {
+      a: polMatrix[3]!,
+      b: polMatrix[7]!,
+      c: polMatrix[11]!,
+    },
+  };
+}
--- src/temperatures.ts
+++ src/temperatures.ts
@@ -3,32 +3,41 @@
  */
 
 /**
- * @param temperature The temperature you want to convert to Celsius
- * @param unit The temperature unit you want to convert
+ * @param {number} temperature The temperature you want to convert to Celsius
+ * @param {TemperatureUnits} [unit] The temperature unit you want to convert
+ *
+ * @returns {number}
  */
 function toCelsius(temperature: number, unit?: TemperatureUnits): number {
   if (unit === 'C') return temperature;
   if (unit === 'K') return Math.floor(temperature - 273.15);
+
   return Math.max(((temperature - 32) * 5) / 9);
 }
 
 /**
- * @param temperature The temperature you want to convert to Celsius
- * @param unit The temperature unit you want to convert
+ * @param {number} temperature The temperature you want to convert to Celsius
+ * @param {TemperatureUnits} [unit] The temperature unit you want to convert
+ *
+ * @returns {number}
  */
 function toFahrenheit(temperature: number, unit?: TemperatureUnits): number {
   if (unit === 'F') return temperature;
   if (unit === 'K') return Math.floor((temperature * 5) / 9 + 459.67);
+
   return Math.max((temperature * 9) / 5 + 32);
 }
 
 /**
- * @param temperature The temperature you want to convert to Celsius
- * @param unit The temperature unit you want to convert
+ * @param {number} temperature The temperature you want to convert to Celsius
+ * @param {TemperatureUnits} [unit] The temperature unit you want to convert
+ *
+ * @returns {number}
  */
 function toKelvin(temperature: number, unit?: TemperatureUnits): number {
   if (unit === 'K') return temperature;
   if (unit === 'F') return Math.floor(((temperature + 459.67) * 5) / 9);
+
   return Math.max(temperature + 273.15);
 }
 
